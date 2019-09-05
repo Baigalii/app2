19fd0f2265573615072fa0e6b96dfb3e2704647f
Baigalii <baigal.gungaadorj@gmail.com>
2019-09-05 17:44:20 +0800

    updated detailpage

--- lib/detail.dart
+++ lib/detail.dart
@@ -26,114 +26,34 @@ class _DetailPageState extends State<DetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Дэлгэрэнгүй мэдээлэл",
-          style: TextStyle(color: Colors.red, fontSize: 15.0),
-        ),
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: Colors.red),
-            onPressed: () {
-              Navigator.pushNamed(context, '/');
-            }),
-        backgroundColor: Colors.white,
-        actions: <Widget>[],
-      ),
-      body: SingleChildScrollView(
-        child: NotificationListener<ScrollNotification>(
-          onNotification: (ScrollNotification notification) {
-            double progress = notification.metrics.pixels /
-                notification.metrics.maxScrollExtent;
-            setState(() {
-              _progress = "${(progress * 100).toInt()}%";
-            });
-            print("BottomEdge: ${notification.metrics.extentAfter == 0}");
-          },
-          child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "but also the leap into electronic typesetting, remaining essentially unchanged. "
-                      "It was popularised in the 1960s with the release of Letraset sheets containing "
-                      "Lorem Ipsum passages, and more recently with desktop publishing software like "
-                      "Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "but also the leap into electronic typesetting, remaining essentially unchanged. "
-                      "It was popularised in the 1960s with the release of Letraset sheets containing "
-                      "Lorem Ipsum passages, and more recently with desktop publishing software like "
-                      "Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "but also the leap into electronic typesetting, remaining essentially unchanged. "
-                      "It was popularised in the 1960s with the release of Letraset sheets containing "
-                      "Lorem Ipsum passages, and more recently with desktop publishing software like "
-                      "Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "but also the leap into electronic typesetting, remaining essentially unchanged. "
-                      "It was popularised in the 1960s with the release of Letraset sheets containing "
-                      "Lorem Ipsum passages, and more recently with desktop publishing software like "
-                      "Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "but also the leap into electronic typesetting, remaining essentially unchanged. "
-                      "It was popularised in the 1960s with the release of Letraset sheets containing "
-                      "Lorem Ipsum passages, and more recently with desktop publishing software like "
-                      "Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "
-                      "but also the leap into electronic typesetting, remaining essentially unchanged. "
-                      "It was popularised in the 1960s with the release of Letraset sheets containing "
-                      "Lorem Ipsum passages, and more recently with desktop publishing software like "
-                      "Aldus PageMaker including versions of Lorem Ipsum."
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                      "Lorem Ipsum has been the industry's standard dummy text ever since the "
-                      "1500s, when an unknown printer took a galley of type and scrambled it "
-                      "to make a type specimen book. It has survived not only five centuries, "),
-                  CircleAvatar(
-                    radius: 30.0,
-                    child: Text(_progress),
-                    backgroundColor: Colors.black54,
-                  ),
-                ],
-              )),
-        ),
+    return Scrollbar(
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          double progress = notification.metrics.pixels /
+              notification.metrics.maxScrollExtent;
+          setState(() {
+            _progress = "${(progress * 100).toInt()}%";
+          });
+          print("BottomEdge: ${notification.metrics.extentAfter == 0}");
+        },
+        child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: Stack(
+              alignment: Alignment.centerRight,
+              children: <Widget>[
+                ListView.builder(
+                    itemCount: 50,
+                    itemExtent: 50.0,
+                    itemBuilder: (context, index) {
+                      return Card(child: ListTile(title: Text("$index")));
+                    }),
+                CircleAvatar(
+                  radius: 30.0,
+                  child: Text(_progress),
+                  backgroundColor: Colors.black54,
+                ),
+              ],
+            )),
       ),
     );
   }
